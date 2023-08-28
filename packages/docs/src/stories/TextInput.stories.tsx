09639aa4d5df0be6e73e562c038bffe91358a0e7
Victor <victor.almeida52@etec.sp.gov.br>
2023-08-27 21:05:22 -0300

    Input component story

---
+++ packages/docs/src/stories/TextInput.stories.tsx
@@ -0,0 +1,38 @@
+import type { StoryObj, Meta } from '@storybook/react'
+
+import { Box, Text, TextInput, TextInputProps } from '@vhalmeida-ui/react'
+
+export default {
+  title: 'Form/TextInput',
+  component: TextInput,
+  args: {},
+  decorators: [
+    (Story) => (
+      <Box
+        as={'label'}
+        css={{ display: 'flex', flexDirection: 'column', gap: '$2' }}
+      >
+        <Text size={'sm'}>Email</Text>
+        {Story()}
+      </Box>
+    ),
+  ],
+} as Meta<TextInputProps>
+
+export const Primary: StoryObj<TextInputProps> = {
+  args: {
+    placeholder: 'Type your name',
+  },
+}
+
+export const Disabled: StoryObj<TextInputProps> = {
+  args: {
+    disabled: true,
+  },
+}
+
+export const WithPrefix: StoryObj<TextInputProps> = {
+  args: {
+    prefix: 'call.com/',
+  },
+}
